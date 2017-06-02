27bc57e36fd7a59c0d16a53ce92df46c4c2a4e7d
Caesar1874 <iwangqinghe@gmail.com>
2017-06-02 15:54:33 +0800

    initial

---
+++ chapter-02/script0.py
@@ -0,0 +1,4 @@
+
+print('hello world')
+print(2 ** 100)
+print('spam ' * 8)
---
+++ chapter-03/script1.py
@@ -0,0 +1,12 @@
+
+# a first python script
+# 载入模块
+import sys
+print(sys.platform)
+
+# 乘方
+print(2 ** 100)
+
+# 字符串重复
+x = 'spam '
+print(x * 8)
